65dd2e0c8abfb89b8477be8eac60378aac1f5372
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-08-03 21:03:20 +1000

    Hero component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import martechLogo from './assets/cloud_computer_pixellove.svg'
-import developerLogo from './assets/keyboard_pixellove.svg'
-import youtubeLogo from './assets/youtube_svgrepo.svg'
 import './App.css'
+import Hero from './components/Hero.jsx'
 
 function App() {
 
   return (
-    <section className='hero-container' >
-      <h1 className='hero-heading'>Braze expert</h1>
-      <h1 className='hero-body'>Creating complete customer experiences with Braze as the engine</h1>
-    </section>
+    <main>
+      <Hero />
+    </main>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+    return (
+    <section className='hero-container' >
+      <h1 className='hero-heading'>Braze architect</h1>
+      <h2 className='hero-body'>inspired by the spirit of <i>Shokunin</i>, crafting flows with mastery and dedication</h2>
+    </section>
+    )
+}
+
+export default Hero
