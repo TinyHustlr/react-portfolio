0e5bb2161edf6c06c9aa16856a8f480142da3326
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-08-04 22:01:45 +1000

    updated badges

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -2,9 +2,11 @@ import brazeDeveloper from '../assets/braze-certified-developer.png'
 import brazeAI from '../assets/braze-certification-brazeai-skills-badge.png'
 import brazePrac from '../assets/braze-certified-practitioner.png'
 import brazeEmail from '../assets/braze-certification-email-deliverability-skills-bad.png'
+import torchieAward from '../assets/braze-torchie-award.png'
 import hackWinner from '../assets/microsoft-global-hackathon-2024-executive-challenge.png'
 import hackathon from '../assets/microsoft-global-hackathon-2023.png'
 import AIRecord from '../assets/ai_guiness_ 1.png'
+
 
 function Certifications() {
     return (
@@ -16,8 +18,9 @@ function Certifications() {
         <img className='cert-image' src={brazeEmail} />
         </div>
         <div className='cert-image-section'>
-        <img className='cert-image' src={hackWinner} />
-        <img className='cert-image' src={hackathon} />
+        <img className='cert-image round-corners' src={torchieAward} />
+        <img className='cert-image round-corners' src={hackWinner} />
+        <img className='cert-image round-corners' src={hackathon} />
         <img className='cert-image' src={AIRecord} />
         </div>
     </div>
