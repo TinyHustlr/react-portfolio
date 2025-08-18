0bdd365e077911aa725a3843cdfa8ab8393b3ae1
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-08-18 17:34:16 +1000

    grid template working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,12 @@ import profilepic from '../assets/profile_pic.png'
 
 function Hero() {
     return (
-    <div className="hero-container">    
+    <div className='hero-container'>    
         <div className='hero-text' >
             <h1 className='hero-heading'>Braze architect</h1>
-            <h2 className='hero-body'>inspired by the spirit of <i>Shokunin</i>, crafting flows with mastery and dedication</h2>
+            <h2 className='hero-body'>inspired by the spirit of <i>Shokunin</i>, balancing mastery, dedication, and play.</h2>
         </div>
-        <div>
+        <div className='hero-image-container'>
             <img className='hero-image' src={profilepic} />
         </div>
     </div>
