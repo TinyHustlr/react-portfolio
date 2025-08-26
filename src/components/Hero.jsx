8affd7f5395a531114dd84a7dd5a0890b0aa17c2
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-08-26 10:12:36 +1000

    fixed CSS styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ function Hero() {
   return (
     <div className="hero-container">
       <div className="hero-text">
-        <h1 className="hero-heading">Braze architect</h1>
-        <h2 className="hero-body">Play | Build | Engage</h2>
+        <h1 className="hero-heading">Play | Build | Engage</h1>
+        <h2 className="hero-body">Braze Architect</h2>
         <br></br>
         <h2 chassName="hero-body">Let's make something remarkable together.</h2>
       </div>
