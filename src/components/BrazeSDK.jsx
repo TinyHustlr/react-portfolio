5e53a6bfa6c22ab29ad9b7f6f6503aa928f1affa
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-08-27 14:13:13 +1000

    Braze CDN test

---
+++ src/components/BrazeSDK.jsx
@@ -0,0 +1,23 @@
+function BrazeSDK() {
+return (
+<>
+{/* // Initialize Braze SDK (loaded via CDN) */}
+braze.initialize('9d6d4815-121a-4429-96c1-37f70e975f01', {
+    baseUrl: 'sdk.fra-02.braze.eu',
+    enableLogging: true,
+    allowUserSuppliedJavascript: false,
+});
+console.log("WEB SDK");
+
+{/* // Open Braze session after a short delay to ensure initialization is complete */}
+setTimeout(() => {
+    braze.changeUser("mvp-test");
+    console.log("CHANGE USER");
+    braze.openSession();
+    console.log("SESSION STARTED");
+}, 1000);
+</>
+)
+}
+
+export default BrazeSDK;
--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Initialize Braze SDK (loaded via CDN)
-braze.initialize('9d6d4815-121a-4429-96c1-37f70e975f01', {
-  baseUrl: 'sdk.fra-02.braze.eu',
-  enableLogging: true,
-  allowUserSuppliedJavascript: false,
-});
-console.log("WEB SDK");
-
-// Open Braze session after a short delay to ensure initialization is complete
-setTimeout(() => {
-  braze.changeUser("mvp-test");
-  console.log("CHANGE USER");
-  braze.openSession();
-  console.log("SESSION STARTED");
-}, 1000);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
