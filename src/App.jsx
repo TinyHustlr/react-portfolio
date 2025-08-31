3c5cf7a6f5d7fa0d944de3fd15d56f25c8c7314e
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-08-31 21:54:03 +1000

    Working on email form

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import CTAButtons from './components/CTAButtons'
 import Ticker from './components/Ticker'
 import YouTube from './components/YouTube'
 import BodyCopy from './components/BodyCopy'
-import EmailForm from './components/EmailForm'
 import Footer from './components/Footer'
+import EmailForm from './components/EmailForm'
 
 function App() {
 
--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,41 +1,45 @@
+import { useState } from 'react';
+
 export default function EmailForm() {
-//   const [result, setResult] = React.useState("");
+  const [result, setResult] = useState("");
 
-//   const onSubmit = async (event) => {
-//     event.preventDefault();
-//     setResult("Sending....");
-//     const formData = new FormData(event.target);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
 
-//     formData.append("access_key", "61d38a4b-321e-41fc-b358-c5c93ed14fc7");
+    formData.append("access_key", "61d38a4b-321e-41fc-b358-c5c93ed14fc7");
 
-//     const response = await fetch("https://api.web3forms.com/submit", {
-//       method: "POST",
-//       body: formData
-//     });
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
 
-//     const data = await response.json();
+    const data = await response.json();
 
-//     if (data.success) {
-//       setResult("Form Submitted Successfully");
-//       event.target.reset();
-//     } else {
-//       console.log("Error", data);
-//       setResult(data.message);
-//     }
-//   };
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" name="name" required/>
-        <input type="email" name="email" required/>
-        <textarea name="message" required></textarea>
-
+        <label for="name">Name</label><br></br>
+        <input type="text" name="name" required/><br></br>
+        <label for="email">Email</label><br></br>
+        <input type="email" name="email" required/><br></br>
+        <label for="message">Message</label><br></br>
+        <textarea name="message" required></textarea><br></br>
+        <div class="h-captcha" data-captcha="true"></div>
         <button type="submit">Submit Form</button>
 
       </form>
-      {/* <span>{result}</span> */}
-
+      <span>{result}</span>
     </div>
   );
 }
