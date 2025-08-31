69b061c14ba0384eff315bd396b776bce4bf8e92
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-08-31 18:21:09 +1000

    updated CSS

--- src/components/BrazeSDK.jsx
+++ src/components/BrazeSDK.jsx
@@ -1,25 +1,25 @@
 /* global braze */
 
-function BrazeSDK() {
-  // Initialize Braze SDK if braze is available
-  if (typeof braze !== 'undefined') {
-    braze.initialize('', {
-        baseUrl: '',
-        enableLogging: false,
-        allowUserSuppliedJavascript: false,
-    });
-    console.log("WEB SDK");
+// function BrazeSDK() {
+//   // Initialize Braze SDK if braze is available
+//   if (typeof braze !== 'undefined') {
+//     braze.initialize('', {
+//         baseUrl: '',
+//         enableLogging: false,
+//         allowUserSuppliedJavascript: false,
+//     });
+//     console.log("WEB SDK");
 
-    // Open Braze session after a short delay to ensure initialization is complete
-    setTimeout(() => {
-        braze.changeUser("mvp-test");
-        console.log("CHANGE USER");
-        braze.openSession();
-        console.log("SESSION STARTED");
-    }, 1000);
-  }
+//     // Open Braze session after a short delay to ensure initialization is complete
+//     setTimeout(() => {
+//         braze.changeUser("mvp-test");
+//         console.log("CHANGE USER");
+//         braze.openSession();
+//         console.log("SESSION STARTED");
+//     }, 1000);
+//   }
 
-  return null; // This component doesn't render anything
-}
+//   return null; // This component doesn't render anything
+// }
 
-export default BrazeSDK;
+// export default BrazeSDK;
