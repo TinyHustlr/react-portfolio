21c17a8848d7de17503262a4dbf203ffd4d71334
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-09-23 21:16:01 +1000

    footer updates and bug fixing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import { reactLogo } from "../assets/react-1.svg";
+import { reactLogo } from "../assets/react-logo.png";
 
 export default function Footer() {
     return (
         <footer className="footer">
             <p>Built with and Vite </p>
-            <img src={ reactLogo } />
+            <img src={ reactLogo } ></img>
         </footer>
     )
 };
