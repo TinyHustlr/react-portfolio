fcfc7d2d9982a2c18ae65f1530c6995268dd6e15
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-09-23 14:54:13 +1000

    Hcatpcha take 2

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react';
+import { useForm } from "react-hook-form";
+import HCaptcha from '@hcaptcha/react-hcaptcha';
 
-export default function EmailForm() {
-  const [result, setResult] = useState("");
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    setResult("Sending....");
-    const formData = new FormData(event.target);
-
-    formData.append("access_key", "61d38a4b-321e-41fc-b358-c5c93ed14fc7");
-
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData
-    });
-
-    const data = await response.json();
-
-    if (data.success) {
-      setResult("Form Submitted Successfully");
-      event.target.reset();
-    } else {
-      console.log("Error", data);
-      setResult(data.message);
-    }
+export default function ContactForm() {
+  const { register, handleSubmit, setValue } = useForm();
+  
+  const onHCaptchaChange = (token) => {
+    setValue("h-captcha-response", token);
   };
+  
+  const onSubmit = async (data) => {
+    console.log(data);
+    
+    await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: data
+    }).then((res) => res.json());
+  }
 
-  return (
-    <div>
-      <form id="contactForm" onSubmit={onSubmit}>
+return (
+  <form onSubmit={handleSubmit(onSubmit)}>
         <label for="name">Name</label><br></br>
         <input type="text" name="name" required/><br></br>
         <label for="email">Email</label><br></br>
@@ -37,9 +27,23 @@ export default function EmailForm() {
         <textarea name="message" required></textarea><br></br>
         <div class="h-captcha" data-captcha="true"></div>
         <button type="submit">Submit Form</button>
+      <HCaptcha
+         sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+         reCaptchaCompat={false}
+         onVerify={onHCaptchaChange} 
+      />
+  </form>
+)}
+
+  return (
+    <div>
+      <form id="contactForm" onSubmit={onSubmit}>
 
       </form>
       <span>{result}</span>
+      <script src="https://web3forms.com/client/script.js" async defer></script>
     </div>
   );
 }
+
+ formData.append("access_key", "61d38a4b-321e-41fc-b358-c5c93ed14fc7");
