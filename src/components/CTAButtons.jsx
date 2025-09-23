0b790b32681ccecb2beca38e9a190c3be6b68837
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-09-23 20:55:53 +1000

    button css updated

--- src/components/CTAButtons.jsx
+++ src/components/CTAButtons.jsx
@@ -5,15 +5,15 @@ export default function CTAButtons() {
         <>
       {/* CTA Buttons */}
         <div className="cta-buttons">
-          <button className="btn-contact" onClick={() => document.getElementById('contactForm').scrollIntoView({ behavior: 'smooth'})}>GET IN TOUCH</button>
+          <button className="btn-contact btn-cta" onClick={() => document.getElementById('contactForm').scrollIntoView({ behavior: 'smooth'})}>GET IN TOUCH</button>
           <a href="https://www.youtube.com/@minimumviablepizza" target="_blank" rel="noreferrer noopener">
-          <button className="btn-youtube">YOUTUBE</button>
+          <button className="btn-youtube btn-cta">YOUTUBE</button>
           </a>
           <a href="https://www.linkedin.com/in/davidworeilly/" target="_blank" rel="noreferrer noopener">
-          <button className="btn-linkedin">LINKEDIN</button>
+          <button className="btn-linkedin btn-cta">LINKEDIN</button>
           </a>
           <a href="https://www.twitch.tv/minimumviablepizza" target="_blank" rel="noreferrer noopener">
-          <button className="btn-twitch">TWITCH</button>
+          <button className="btn-twitch btn-cta">TWITCH</button>
           </a>
         </div>
         </>
