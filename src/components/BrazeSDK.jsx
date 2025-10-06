7c3c3275673f8f797dc17b9e430700ba0f75676e
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-10-06 15:08:55 +1100

    SDK Push In app and card test

--- src/components/BrazeSDK.jsx
+++ src/components/BrazeSDK.jsx
@@ -1,23 +1,23 @@
 // /* global braze */
 
-// export default function BrazeSDK() {
-//   // Initialize Braze SDK if braze is available
-//   if (typeof braze !== 'undefined') {
-//     braze.initialize('9d6d4815-121a-4429-96c1-37f70e975f01', {
-//         baseUrl: 'sdk.fra-02.braze.eu',
-//         enableLogging: false,
-//         allowUserSuppliedJavascript: false,
-//     });
-//     console.log("Web SDK initialized");
+export default function BrazeSDK() {
+  // Initialize Braze SDK if braze is available
+  if (typeof braze !== 'undefined') {
+    braze.initialize('9d6d4815-121a-4429-96c1-37f70e975f01', {
+        baseUrl: 'sdk.fra-02.braze.eu',
+        enableLogging: false,
+        allowUserSuppliedJavascript: false,
+    });
+    console.log("Web SDK initialized");
 
-//     // Open Braze session after a short delay to ensure initialization is complete
-//     setTimeout(() => {
-//         braze.changeUser("mvp-test");
-//         console.log("User changed");
-//         braze.openSession();
-//         console.log("Session started");
-//     }, 1000);
-//   }
+    // Open Braze session after a short delay to ensure initialization is complete
+    setTimeout(() => {
+        braze.changeUser("mvp-test");
+        console.log("User changed");
+        braze.openSession();
+        console.log("Session started");
+    }, 1000);
+  }
 
-//   return null;
-// }
+  return null;
+}
