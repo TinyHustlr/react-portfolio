443c27d4a771c7f416df8607968fac8046347c3e
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-10-29 15:07:43 +1100

    working on SDK debugging

--- src/components/BrazeSDK.jsx
+++ src/components/BrazeSDK.jsx
@@ -1,28 +1,12 @@
 // /* global braze */
 
 // export default function BrazeSDK() {
-//   // Initialize Braze SDK if braze is available
-//   if (typeof braze !== 'undefined') {
-//     braze.initialize('import.meta.env.VITE_BRAZE_API_KEY', {
-//         baseUrl: 'sdk.fra-02.braze.eu',
-//         enableLogging: true,
-//         allowUserSuppliedJavascript: false,
-//     });
-//     console.log("Web SDK initialized");
+
 
 //     // optionally show all in-app messages without custom handling
 //     braze.automaticallyShowInAppMessages();
 //     console.log("In app message to show")
 
-
-//     // Open Braze session after a short delay to ensure initialization is complete
-//     setTimeout(() => {  
-//         braze.changeUser("mvp-test");
-//         console.log("User changed");
-//         braze.openSession();
-//         console.log("Session started");
-//     }, 1000);
-//   }
 
 //   return null;
 // }
