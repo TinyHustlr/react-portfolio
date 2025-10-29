12215861a7e803e40c7c090d9efbb4f2674cd020
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-10-29 15:01:45 +1100

    working on SDK debugging

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,13 @@ import App from './App.jsx'
 import * as braze from '@braze/web-sdk'
 
 // Initialize Braze SDK
-braze.initialize('import.meta.env.VITE_BRAZE_API_KEY', {
+braze.initialize('9d6d4815-121a-4429-96c1-37f70e975f01', {
     baseUrl: 'https://sdk.fra-02.braze.eu',
     enableLogging: true,
     allowUserSuppliedJavascript: false,
 });
 console.log("Web SDK initialized");
 
-// Open Braze session after a short delay to ensure initialization is complete
-    braze.changeUser("mvp-test");
-    console.log("User changed")
-    braze.openSession();
-    console.log("Session open")
-  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
