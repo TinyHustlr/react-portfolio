1f96e2128730b622c65004c4ddd4c95a747aa53a
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-11-07 15:58:20 +1100

    Content card updates. Bug fixing

--- src/components/ContentCards.jsx
+++ src/components/ContentCards.jsx
@@ -3,12 +3,13 @@ import * as braze from "@braze/web-sdk";
 
 export default function ContentCards() {
   useEffect(() => {
-    // Subscribe to updates
-    braze.showContentCards("feed");
-
-    // Request cards from Braze
-    braze.requestContentCardsRefresh();
+    // Show cards
+    braze.showContentCards(document.getElementById('content-cards'));
   }, []);
 
-  return <div id="feed"></div>;
+  return (
+      <>
+      <div id={'content-cards'} />
+      </>
+  );
 }
