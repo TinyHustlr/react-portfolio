a0538e22a08a268a7e37185a7579e6cee96eb124
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2025-12-08 12:24:58 +1100

    working on react router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import Certifications from './components/Certifications';
-import Hero from './components/Hero';
-import CTAButtons from './components/CTAButtons';
-import Ticker from './components/Ticker';
-import YouTube from './components/YouTube';
-import BodyCopy from './components/BodyCopy';
-import Footer from './components/Footer';
-import EmailForm from './components/EmailForm';
+import Certifications from '../components/Certifications';
+import Hero from '../components/Hero';
+import CTAButtons from '../components/CTAButtons';
+import Ticker from '../components/Ticker';
+import YouTube from '../components/YouTube';
+import BodyCopy from '../components/BodyCopy';
+import Footer from '../components/Footer';
+import EmailForm from '../components/EmailForm';
 
 function Home() {
 
@@ -23,4 +23,4 @@ function Home() {
   );
 }
 
-export default Home
+export default Home;
