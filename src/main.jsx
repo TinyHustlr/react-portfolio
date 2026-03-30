17a16ef04040111b7dbf0dbb1f39d2abf8fb55d7
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2026-03-30 22:10:17 +1100

    braze on

--- src/main.jsx
+++ src/main.jsx
@@ -5,40 +5,40 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import * as braze from "@braze/web-sdk";
 
-// function BrazeInitializer() {
-//   useEffect(() => {
-//     console.log("Initializing Braze...");
+function BrazeInitializer() {
+  useEffect(() => {
+    console.log("Initializing Braze...");
 
-//     braze.initialize("75afc0ca-d5da-4a7a-8f55-c5226d116349", {
-//       baseUrl: "https://sdk.fra-02.braze.eu",
-//       enableLogging: false,
-//       allowUserSuppliedJavascript: true,
-//     });
+    braze.initialize("f1cc2684-5ef1-46ed-b0d9-f4a047f3a6b1", {
+      baseUrl: "https://sdk.fra-02.braze.eu",
+      enableLogging: false,
+      allowUserSuppliedJavascript: true,
+    });
 
-//     braze.openSession();
-//     console.log("Session opened");
+    braze.openSession();
+    console.log("Session opened");
 
-//     braze.changeUser("mvp-test");
-//     console.log("User changed to mvp-test");
+    braze.changeUser("mvp-test");
+    console.log("User changed to mvp-test");
 
-//     setTimeout(() => {
-//       braze.automaticallyShowInAppMessages();
-//       console.log("In-app messages enabled");
-//     }, 1000);
-
-
-//     setTimeout(() => {
-//       braze.requestPushPermission();
-//       console.log("Requesting push permission v2");
-//     }, 5000);
+    setTimeout(() => {
+      braze.automaticallyShowInAppMessages();
+      console.log("In-app messages enabled");
+    }, 1000);
 
 
+    setTimeout(() => {
+      braze.requestPushPermission();
+      console.log("Requesting push permission v2");
+    }, 5000);
 
-//     window.braze = braze;
-//   }, []);
 
-//   return null;
-// }
+
+    window.braze = braze;
+  }, []);
+
+  return null;
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -49,7 +49,3 @@ createRoot(document.getElementById("root")).render(
     </>
   </StrictMode>
 );
-
-
-
-
