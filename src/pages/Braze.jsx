29073a8762f1c5b3399bf86c67b1163bef8a86db
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2026-04-08 14:18:57 +1000

    Email input added

--- src/pages/Braze.jsx
+++ src/pages/Braze.jsx
@@ -1,7 +1,11 @@
 function Braze() {
 
   return (
-    <h1>NEW TEST PAGE AHOY!</h1>
+    <div>
+      <h1>BRAZE TEST PAGE AHOY!🚢</h1>
+      <label for="email-el">EMAIL</label>
+      <input id="email-el" name="email-el" type="email" placeholder="Enter email" />
+    </div>
   );
 }
 
