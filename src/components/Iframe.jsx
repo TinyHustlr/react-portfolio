75dd19b3040e41a70ce6c087f27e193128cdbbf8
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2026-09-02 12:12:13 +1000

    iframe test

---
+++ src/components/Iframe.jsx
@@ -0,0 +1,8 @@
+export default function Iframe() {
+  return (
+    <iframe
+      src="https://example.com"
+      style={{ width: "100%", maxWidth: "800px", height: "500px", border: "none", borderRadius: "8px" }}
+    ></iframe>
+  );
+};
