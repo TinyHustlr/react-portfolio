6f42170e98c1af4757a8422ffc4d3cb4b29e8d35
David O'Reilly <91208042+TinyHustlr@users.noreply.github.com>
2026-09-02 12:14:54 +1000

    iframe test

--- src/components/Iframe.jsx
+++ src/components/Iframe.jsx
@@ -1,7 +1,7 @@
 export default function Iframe() {
   return (
     <iframe
-      src="https://fb6491b6-cada-4140-bbc2-70232ca4b9d5.eu02.ldpgs.eu/7cqym"
+      src="https://playbuildengage.com"
       style={{ width: "100%", maxWidth: "800px", height: "500px", border: "none", borderRadius: "8px" }}
     ></iframe>
   );
